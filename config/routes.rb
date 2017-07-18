a0850c24b9c8c4759e2a1c6f8d22308b572409a0
Tianming Zhuang <tianming.zhuang@gmail.com>
2017-07-18 14:48:55 -0400

    Fix routing issue

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :compare_ranges
-  resources :known_issues
+  resources :known_issues, path: 'screendump_pr_entries'
   resources :screendump_results
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
